8b18e36743647ce9db13026a395efa41f6144bc2
emirdon <emirseptiansorid@gmail.com>
2019-03-15 02:31:29 +0700

    Initial commit

---
+++ app/src/main/java/restuibu/com/itmarket/notifications/Constants.java
@@ -0,0 +1,9 @@
+package restuibu.com.itmarket.notifications;
+
+public class Constants {
+
+    public static final String FIREBASE_TOKEN = "firebase token";
+    public static final String CHANNEL_ID = "my_channel_01";
+    public static final String CHANNEL_NAME = "Simplified Coding Notification";
+    public static final String CHANNEL_DESCRIPTION = "www.simplifiedcoding.net";
+}
---
+++ app/src/main/java/restuibu/com/itmarket/notifications/MyFirebaseMessagingService.java
@@ -0,0 +1,54 @@
+package restuibu.com.itmarket.notifications;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Random;
+
+import restuibu.com.itmarket.R;
+import restuibu.com.itmarket.notifications.StartActivity;
+
+/**
+ * Created by L on 09/05/2017.
+ * Copyright (c) 2017 Centroida. All rights reserved.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        //if the message contains data payload
+        //It is a map of custom keyvalues
+        //we can read it easily
+        if(remoteMessage.getData().size() > 0){
+            //handle the data message here
+        }
+
+        //getting the title and the body
+        String title = remoteMessage.getNotification().getTitle();
+        String body = remoteMessage.getNotification().getBody();
+
+        //then here we can use the title and body to build a notification
+        MyNotificationManager.getInstance(this).displayNotification(title, body);
+    }
+}
